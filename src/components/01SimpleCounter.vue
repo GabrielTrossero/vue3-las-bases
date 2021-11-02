6538826c86288d3624bba76754f3680f53449bd3
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2021-11-02 11:13:13 -0300

    simple counter

---
+++ src/components/01SimpleCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Contador Sencillo</h2>
+  <p>{{ counter }}</p>
+  <button @click="increment">Incrementar</button>
+  <button @click="decrement">Decrementar</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "SimpleCounter",
+
+  setup() {
+    let counter = ref(0);
+
+    const increment = () => {
+      counter.value++;
+    };
+
+    const decrement = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      increment,
+      decrement,
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
+  <SimpleCounter />
+  <hr />
   <HelloWorld />
 </template>
 
 <script>
 // @ is an alias to /src
+import SimpleCounter from "@/components/01SimpleCounter.vue";
 import HelloWorld from "@/components/00HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
+    SimpleCounter,
     HelloWorld,
   },
 };
