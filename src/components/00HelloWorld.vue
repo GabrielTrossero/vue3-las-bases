5482a5601469a42654e690434a9e8260bb5aab20
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2021-11-02 09:28:05 -0300

    use of v-model

---
+++ src/components/00HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Hola Mundo</h2>
+  <p>{{ messaje }}</p>
+  <input v-model="messaje" />
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "HelloWorld",
+
+  setup() {
+    const messaje = ref("Hello World");
+
+    return {
+      messaje,
+    };
+  },
+};
+</script>
+
+<style></style>
