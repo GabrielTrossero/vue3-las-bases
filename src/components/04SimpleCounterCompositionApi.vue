a22d1d242eb01bd85516065dd69a86de2102acb7
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2021-11-03 20:31:20 -0300

    counter component refactoring

---
+++ src/components/04SimpleCounterCompositionApi.vue
@@ -0,0 +1,26 @@
+<!--
+    Este componente es una ejemplificación de como podríamos hacer
+    para reutilizar la logica del counter.
+    Se llaman a las funciones que están en useCounter.js
+-->
+<template>
+  <h2>Simple Counter Composition API</h2>
+  <p>{{ counter }}</p>
+  <button @click="increment">Incrementar</button>
+  <button @click="decrement">Decrementar</button>
+</template>
+
+<script>
+import useCounter from "../composables/useCounter";
+export default {
+  name: "SimpleCounterCompositionApi",
+
+  setup() {
+    const { counter, increment, decrement } = useCounter();
+
+    return { counter, increment, decrement };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
+  <SimpleCounterCompositionApi />
+  <hr />
   <WatchCounter />
   <hr />
   <ReactiveCounter />
@@ -10,6 +12,7 @@
 
 <script>
 // @ is an alias to /src
+import SimpleCounterCompositionApi from "@/components/04SimpleCounterCompositionApi.vue";
 import WatchCounter from "@/components/03WatchCounter.vue";
 import ReactiveCounter from "@/components/02ReactiveCounter.vue";
 import SimpleCounter from "@/components/01SimpleCounter.vue";
@@ -18,6 +21,7 @@ import HelloWorld from "@/components/00HelloWorld.vue";
 export default {
   name: "Home",
   components: {
+    SimpleCounterCompositionApi,
     WatchCounter,
     ReactiveCounter,
     SimpleCounter,
